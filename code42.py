5d50a0e49a00c6e167a1aab14f7da7e7d48c05d2
Al-Jamil Suvo <aljamilsuvo@gmail.com>
2019-09-07 11:02:22 +0600

    code45

---
+++ code42.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def trap(self, height):
+        print(height)
+        left=0
+        right=len(height)-1
+        left_max=0
+        right_max=0
+        ans=0
+        while left<right:
+            if height[left]<height[right]:
+                if height[left]>=left_max:
+                    left_max=height[left]
+                else:
+                    ans+=left_max-height[left]
+                left+=1
+            else:
+                if height[right]>=right_max:
+                    right_max=height[right]
+                else:
+                    ans+=right_max-height[right]
+                right-=1
+        return ans
+
+
+s=Solution()
+print(s.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
---
+++ code45.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def jump(self, nums):
+        current=0
+        jump_cnt=0
+        target=len(nums)-1
+        while  True:
+            if current>=target:
+                return jump_cnt
+            current_max_jumps=nums[current]
+            max_index=0
+            next_max_jumps=-1
+            for index in range(current+1,current+current_max_jumps+1):
+                if index>=target:
+                    return jump_cnt+1
+                if nums[index]+index>=next_max_jumps:
+                    max_index=index
+                    next_max_jumps=nums[index]+index
+            current=max_index
+            jump_cnt+=1
+s=Solution()
+print(s.jump([10,9,8,7,6,5,4,3,2,1,1,0]))
+
+
+
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name
