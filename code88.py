e4bd6b922831c7ac17f27c288194284af181a14d
Al-Jamil Suvo <aljamilsuvo@gmail.com>
2019-12-08 21:38:36 +0600

    code88

--- code88.py
+++ code88.py
@@ -7,17 +7,14 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        nums1=nums1[:m]
-        nums2=nums2[:n]
-
         i=0
         j=0
         while j<n:
-            print(i,j,nums1)
-            if i>
-            if i<m and  nums2[j]<nums1[i] :
+            if i>=m+j or nums2[j]<nums1[i]:
+                del nums1[-1]
                 nums1.insert(i,nums2[j])
                 j+=1
             i+=1
+        print(nums1)
 s=Solution()
-print(s.merge([1,2,3,0,0,0],3,[2,5,6],3))
+s.merge([1,2,3,0,0,0],3,[2,5,6],3)
